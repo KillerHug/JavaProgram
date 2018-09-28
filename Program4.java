d1e6ae5a85f78c2c4b34b2cb2c0655cc4d548543
KillerHug <43376826+KillerHug@users.noreply.github.com>
2018-09-28 20:51:13 +0530

    Add files via upload

---
+++ Program4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Program4
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s;
+		String s1=".";
+		String rev="";
+
+		s=sc.nextLine();
+		rev=s.concat(s1);
+		System.out.println(rev);
+
+	}
+}
