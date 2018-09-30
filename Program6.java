9e819d0fd4a17ec449a800274cf32551d48ffe15
KillerHug <43376826+KillerHug@users.noreply.github.com>
2018-09-30 10:16:41 +0530

    Add files via upload

---
+++ Program6.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Program6
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s;
+		
+
+		s=sc.next();
+		if(s.equals("Sunday"))
+		{
+				System.out.println("Yes");
+		}
+			else
+			{
+				System.out.println("No");
+			}
+
+	}
+}
